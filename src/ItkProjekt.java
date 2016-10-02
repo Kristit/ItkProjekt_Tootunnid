6db57ad6e313218158a7a9637b468397ce90dbe1
kristitammet <kristitammet@gmail.com>
2016-10-02 16:33:32 +0300

    muudatused

--- src/ItkProjekt.java
+++ src/ItkProjekt.java
@@ -4,29 +4,45 @@
  */
 
 
+
         import java.util.Scanner; /*selle pead sa alati enen public folderit sisse tooma,
         sest see annab programmile teada, et sa kasutad liberist scanneri koodijuppi*/
 
+
+
+
         public class ItkProjekt {
             public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);// Kas see igakord saab uue sisendi?
                 System.out.println("Teretulemast töötudnide arvestus programmi!");
-                    String kursuseNimi;
-                    //int aineMaht
+                    String kursuseNimi; // defineerin variabalid
+                    int aineMaht;
+                    int sum;
 
-                //allows user to enter subject name
-                    Scanner one= new Scanner(System.in);
+                //asks user to enter subject name
+                    Scanner one= new Scanner(System.in);// kuidas saab seda kirjutada nii et ei see allub ulemisele reale?
                 System.out.println( "Sisesta oma õpitava aine nimi:");
-                kursuseNimi =one.nextLine(); // string=sona, kursuenimi= variable,input= isestatud info, next(line)=kuvab uuel real
+                kursuseNimi =one.next(); // kursuenimi= variable, next=kuvab uuel real
 
-                //allows user to enter subject volume (AP)
+                //asks user to enter subject volume (AP)
+                    Scanner second= new Scanner (System.in);
                 System.out.println("Sisesta aine mahu AP-d:");
-                    String aineMaht=input.next();
+                    aineMaht=second.nextInt();// selle pean tapsutama, et on int
+                    sum= aineMaht*20;
+            System.out.println("Sinu töötundide arv " + kursuseNimi +" kursusel on: "+ sum+" tundi");
 
-            System.out.println("Sinu töötundide arv selle arine raames on:" + kursuseNimi);
+            // nuud peaks vsit objective hakakma kasutama?
 
+            // asks user to enter task & deadlines
+                String Ulesanne;
+                System.out.println("Palun sisesta ülesanne ja tähtaeg!"+"\n");
+                System.out.println("Esimene ülesanne:");
+                Scanner i= new Scanner(System.in);
+                Ulesanne=i.next();
 
-
+             //dealine, uleval kaustan Java Dateformatit
+                System.out.println("ja selle tähtaeg:");
+                Scanner scanner= new Scanner(System.in);
 
 
 
