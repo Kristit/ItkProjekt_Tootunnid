11535783d42f65e03dca97c3d471ee03199390a5
kristitammet <kristitammet@gmail.com>
2016-10-02 20:28:31 +0300

    Tunide arv

--- src/ItkProjekt.java
+++ src/ItkProjekt.java
@@ -5,6 +5,8 @@
 
 
 
+        import com.sun.deploy.panel.ExceptionListDialog;
+
         import java.util.Calendar;
         import java.util.GregorianCalendar;
         import java.util.Scanner; /*selle pead sa alati enen public folderit sisse tooma,
@@ -19,43 +21,54 @@
 
         public class ItkProjekt {
             public static void main(String[] args) throws ParseException {
-            Scanner input = new Scanner(System.in);// Kas see igakord saab uue sisendi?
+                Scanner input = new Scanner(System.in);// Kas see igakord saab uue sisendi?
                 System.out.println("Teretulemast töötudnide arvestus programmi!");
-                    String kursuseNimi; // defineerin variabalid
-                    int aineMaht;
-                    int sum;
+                String kursuseNimi; // defineerin variabalid
+                int aineMaht;
+                int sum;
 
                 //asks user to enter subject name
-                    Scanner one= new Scanner(System.in);// kuidas saab seda kirjutada nii et ei see allub ulemisele reale?
-                System.out.println( "Sisesta oma õpitava aine nimi:");
-                kursuseNimi =one.next(); // kursuenimi= variable, next=kuvab uuel real
+                Scanner one = new Scanner(System.in);// kuidas saab seda kirjutada nii et ei see allub ulemisele reale?
+                System.out.println("Sisesta oma õpitava aine nimi:");
+                kursuseNimi = one.next(); // kursuenimi= variable, next=kuvab uuel real
 
                 //asks user to enter subject volume (AP)
-                    Scanner second= new Scanner (System.in);
+                Scanner second = new Scanner(System.in);
                 System.out.println("Sisesta aine mahu AP-d:");
-                    aineMaht=second.nextInt();// selle pean tapsutama, et on int
-                    sum= aineMaht*20;
-            System.out.println("Sinu töötundide arv " + kursuseNimi +" kursusel on: "+ sum+" tundi");
+                aineMaht = second.nextInt();// selle pean tapsutama, et on int
+                sum = aineMaht * 20;
 
 
-            // asks user how many tasks he/she has
-                String askTaskNumber;
-                Scanner third= new Scanner (System.in);
-                System.out.println("Sisesta mitu erinevat taski Sul on: ");
-                askTaskNumber= third.next();
+                // asks user how many tasks he/she has
+                int askTaskNumber;
+                Scanner third = new Scanner(System.in);
+                System.out.println("Sisesta mitu erinevat taski Sul kursuse jooksul on: ");
+                askTaskNumber = third.nextInt();
 
-            // asks user to enter tasks
-                String task;
-                System.out.println("Palun sisesta ülesannded"+"\n");
-                Scanner input = new Scanner(System.in);*/
+                // asks user to enter tasks and how many hours he/she wants to spend time?
+                String askTaskNimi;
+                Scanner fourth = new Scanner(System.in);
+                System.out.println("Sinu kogu töötundide arv " + kursuseNimi + " kursusel on: " + sum + " tundi" + "\n");
+                for (int i = 0; i < askTaskNumber; i++) {
+                    System.out.println("Palun sisesta task " + (1 + i) + ":");
+                    askTaskNimi = fourth.nextLine();
+                    System.out.println("Palun sisesta task " + (1 + i) + " tundide arv: ");
+
+                int askTaskHours;
+                Scanner fifth = new Scanner(System.in);
+                    askTaskHours = fifth.nextInt();
+                    int leftHours;
+                    leftHours= sum-askTaskHours;
+                    System.out.println("Järgijäänud töötundide maht on: " + leftHours);
+                    if (leftHours<=0){
+                        System.out.println("Vaata tundide arv üle! Error!");
+
+
+                    }
 
 
 
-
-
-                // asks user deadline
-                //Scanner scanner = new Scanner(System.in);
-                //System.out.println("Lisa ülesande tahtaeg: ");
+            }
 
 
 
