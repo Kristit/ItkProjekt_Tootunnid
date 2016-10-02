bb24b6087d43dba6ed6fa0f672210b431e6bbc25
kristitammet <kristitammet@gmail.com>
2016-10-02 13:18:16 +0300

    Initial commit

---
+++ src/ItkProjekt.java
@@ -0,0 +1,13 @@
+/**ITK Projekt
+ * Tootundied jaotus ja lugemine
+ * Created by kristitammet on 02/10/16.
+ */
+    public class ItkProjekt {
+        public static void main(String[] args) {
+        System.out.print("Teretulemast töötudnide arvestus programmi");
+
+
+
+
+    }
+}
