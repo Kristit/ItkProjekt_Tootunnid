0cdeecc390360381036143cb532ba1c4d6a47fce
kristitammet <kristitammet@gmail.com>
2016-10-09 14:01:39 +0300

    Shorten the code, added extra Task class and made an array list from it, splited code into models

--- src/ItkProjekt.java
+++ src/ItkProjekt.java
@@ -2,60 +2,74 @@
  * Tootundied jaotus ja lugemine
  * Created by kristitammet on 02/10/16.
  */
+import java.util.ArrayList;
+import java.util.Scanner; /*selle pead sa alati enen public folderit sisse tooma,
+sest see annab programmile teada, et sa kasutad liberist scanneri koodijuppi*/
 
+import java.text.ParseException;
 
+public class ItkProjekt {
+    public static void main(String[] args) throws ParseException {
+        Scanner input = new Scanner(System.in);// sobib kogu programmile =>tahan siin kasutada Java oma koodi juppi)
+        System.out.println("Teretulemast töötundide arvestus programmi!" + "\n");
+        String courseName; // defineerin variabalid
+        int credits;
+        int leftHours;
 
-        import com.sun.deploy.panel.ExceptionListDialog;
-        import java.util.Scanner; /*selle pead sa alati enen public folderit sisse tooma,
-        sest see annab programmile teada, et sa kasutad liberist scanneri koodijuppi*/
+        //asks user to enter subject name
+        System.out.println("Sisesta oma õpitava aine nimi:");
+        courseName = input.next();
 
-        import java.text.ParseException;
+        // TODO: check if this course has already been saved to a file and in that case load the data from that file
 
+        //asks user to enter subject volume (AP)
+        System.out.println("Sisesta aine mahu AP-d:");
+        credits = input.nextInt();
+        leftHours = credits * 20;
 
-        public class ItkProjekt{
-            public static void main(String[] args) throws ParseException {
-                Scanner input = new Scanner(System.in);// sobib kogu programmile =>tahan siin kasutada Java oma koodi juppi)
-                System.out.println("Teretulemast töötundide arvestus programmi!" + "\n");
-                String courseName; // defineerin variabalid
-                int credits;
-                int leftHours;
+        // asks user how many tasks he/she has
+        System.out.println("Sisesta mitu erinevat taski Sul kursuse jooksul on: ");
+        int askedTaskNumber = input.nextInt();
 
-                //asks user to enter subject name
-               System.out.println("Sisesta oma õpitava aine nimi:");
-                courseName = input.next();
+        // asks user to enter tasks and how many hours he/she wants to spend time?
+        System.out.println("Sinu kogu töötundide arv " + courseName + " kursusel on: " + leftHours + " tundi\n");
 
-                //asks user to enter subject volume (AP)
-                System.out.println("Sisesta aine mahu AP-d:");
-                credits = input.nextInt();
-                leftHours = credits * 20;
+        ArrayList<Task> taskList = readTaskList(input, askedTaskNumber, leftHours);
 
-
-                // asks user how many tasks he/she has
-                int askTaskNumber;
-                System.out.println("Sisesta mitu erinevat taski Sul kursuse jooksul on: ");
-                askTaskNumber = input.nextInt();
-
-                // asks user to enter tasks and how many hours he/she wants to spend time?
-                String askTaskName;
-                System.out.println("Sinu kogu töötundide arv " + courseName + " kursusel on: " + leftHours + " tundi" + "\n");
-
-
-                for (int i = 0; i < askTaskNumber; i++) {
-                    System.out.println("Palun sisesta task " + (1 + i) + ":");
-                    askTaskName = input.next();
-
-                    System.out.println( "Palun sisesta tundide arv: " );
-                    int askTaskHours;
-                    askTaskHours = input.nextInt();
-                    leftHours = leftHours - askTaskHours;
-                    System.out.println("\n"+ "Järgijäänud töötundide maht on: " + leftHours);
-                    if (leftHours < 0) {
-                        System.out.println("Vaata tundide arv üle! Error!");
-
-
-                    }
-
-                }
-
-            }
+        System.out.println("\nExcellent! You have: " + taskList.size() + " tasks"); // fn F2 shows descripiton of anything
+        for (int i=0; i<taskList.size(); i++) {
+            System.out.println("\nTask " + (i+1) + " is " + taskList.get(i).name + " and has " + taskList.get(i).hours + " hours");
         }
+
+        // TODO: save the course (with task list) to a file
+    }
+
+    private static ArrayList<Task> readTaskList(Scanner input, int askedTaskNumber, int leftHours) {
+        ArrayList<Task> taskList = new ArrayList<Task>(); /* we have array list and we call it taskList, we
+        creat a new arrayList of tasks object*/
+
+        for (int i = 1; i <= askedTaskNumber; i++) {
+            Task task = new Task();
+            System.out.println("Palun sisesta task " + i + ":");
+            task.name = input.next();// as it is string, you don't need a next String!! (Intiga is nessery)
+
+            if (i < askedTaskNumber) {
+                if (i>1) System.out.println("\nJärgijäänud töötundide maht on: " + leftHours);
+                System.out.println( "Palun sisesta tundide arv: " );
+                task.hours = input.nextInt(); // luhendad rida
+                if (task.hours > leftHours) {
+                    System.out.println("Vaata tundide arv üle! Error!");
+                    i--; // make i same as it was the current task
+                    continue; // contnniu for loop
+                }
+                leftHours = leftHours - task.hours; //  if there no error, only then save it ..Refector->rename variables all
+            } else {
+                task.hours = leftHours; // that we get last hours form system caluclation
+                System.out.println("Töötundide maht on: "+ leftHours);
+            }
+            taskList.add(task); // we collected all datas and now puting in the taskList
+        }
+
+        return taskList;
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+/**
+ * Created by kristitammet on 08/10/16.
+ */
+public class Task {
+    int hours; // this for task hours
+    String name; //this is for task name
+    LocalDate date;
+}
